2220fe03570ef5488a45c79355f92a84d2af075c
Siarhei Naralenkau <naralenkov2010@gmail.com>
2021-03-03 20:43:27 +0300

    Initial project configuration for resources & viewBinding usage

--- app/src/main/java/by/itacademy/notebookscatalog/fragments/MainActivity.kt
+++ app/src/main/java/by/itacademy/notebookscatalog/fragments/MainActivity.kt
@@ -2,10 +2,16 @@ package by.itacademy.notebookscatalog.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.itacademy.notebookscatalog.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = binding.root
+        setContentView(rootView)
     }
 }
