60e96af41dca69870fcdd93d5095092734eaedb0
Siarhei Naralenkau <siarhei.naralenkau@godel.shellenergy.co.uk>
2021-03-04 09:45:10 +0300

    Adding Fragment to add device

--- app/src/main/java/by/itacademy/notebookscatalog/fragments/MainActivity.kt
+++ app/src/main/java/by/itacademy/notebookscatalog/fragments/MainActivity.kt
@@ -2,13 +2,17 @@ package by.itacademy.notebookscatalog.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import by.itacademy.notebookscatalog.fragments.databinding.ActivityMainBinding
+import by.itacademy.notebookscatalog.fragments.ui.DeviceAddFragment
 import by.itacademy.notebookscatalog.fragments.ui.DevicesListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fDevicesList: DevicesListFragment
+    private lateinit var fDeviceAdd: DeviceAddFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +20,18 @@ class MainActivity : AppCompatActivity() {
         val rootView = binding.root
         setContentView(rootView)
 
-        val fragmentList = DevicesListFragment()
-        setCurrentFragment(fragmentList)
+        fDevicesList = DevicesListFragment()
+        fDeviceAdd = DeviceAddFragment()
+        setCurrentFragment(fDevicesList)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.miHome -> setCurrentFragment(fDevicesList)
+                R.id.miAdd -> setCurrentFragment(fDeviceAdd)
+                R.id.miProfile -> Toast.makeText(this, "Switch to Profile Fragment", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 
     private fun setCurrentFragment(fragment: Fragment) =
---
+++ app/src/main/java/by/itacademy/notebookscatalog/fragments/ui/DeviceAddFragment.kt
@@ -0,0 +1,38 @@
+package by.itacademy.notebookscatalog.fragments.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.itacademy.notebookscatalog.fragments.R
+import by.itacademy.notebookscatalog.fragments.databinding.FragmentDeviceAddBinding
+
+class DeviceAddFragment : Fragment(R.layout.fragment_device_add) {
+
+    private val logTag = "DeviceAddFragment"
+
+    private var _binding: FragmentDeviceAddBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        Log.d(logTag, "onAttach is called")
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(logTag, "onCreateView is called")
+        _binding = FragmentDeviceAddBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
